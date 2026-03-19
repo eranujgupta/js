a33e78445448b14f30b6e329bdd447bf703acb5c
Anuj Gupta <eranujgupta1984@gmail.com>
2026-03-19 14:43:13 +0000

    variable type

---
+++ 01basics/01_Variables.js
@@ -0,0 +1,6 @@
+const accountId = 144553
+let accountEmail = "eranujgupta1984@gmail.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+console.log(accountId)
+//console.table([accountId, accountEmail, accountPassword, accountCity])
